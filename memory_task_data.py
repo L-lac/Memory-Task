6aa51f4336bf3297feceb9b22677cd66716604db
Lena <lena.lin.412@Gmail.com>
2025-01-15 05:49:32 -0500

    Update memory_task_data.py

--- memory_task_data.py
+++ memory_task_data.py
@@ -123,14 +123,14 @@ for run in data['Run'].unique():
       ws.cell(row=num_row, column=num_col, value=value)
 
   #Creating "Study Phase" header + Leaves gap between two phases 
-  study_start_col = len(recognition_columns) + 2  # Leave a gap of 1 column
+  study_start_col = len(recognition_columns) + 2  
   ws.merge_cells(start_row=1, start_column=study_start_col, end_row=1, end_column=study_start_col + len(study_columns) - 1)
   ws.cell(row=1, column=study_start_col, value="Study Phase")
   ws.cell(row=1, column=study_start_col).alignment = Alignment(horizontal='center')
 
   #Adding in Study Phase data
   for num_row, row in enumerate(dataframe_to_rows(study_data[study_columns], index=False, header=True), start=2):
-    for num_col, value in enumerate(row, start=study_start_col):  # Start writing at the study_start_col
+    for num_col, value in enumerate(row, start=study_start_col):  
       ws.cell(row=num_row, column=num_col, value=value)
 
   #Saving the workbook
